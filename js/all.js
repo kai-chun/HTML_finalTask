9448b59bf6bb8ece181670b9bd10b902b42ef695
Kai-Chun Yang <kelly21225@gmail.com>
2022-03-29 21:15:40 +0800

    update: add jQuery

---
+++ js/all.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    // dropdown ul
+    $('.dropClick').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $(this).siblings().slideToggle();
+        $(this).parent().siblings().find('.dropClick').removeClass("active");
+        $(this).parent().siblings().find('.dropdown').slideUp();
+    });
+
+    // swiper
+    const swiper = new Swiper(".swiper", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        speed: 2000,
+        autoplay: {
+            delay: 2000,
+        },
+      
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        }
+    });
+
+    // lightbox
+    lightbox.option({
+        resizeDuration: 500,
+        wrapAround: true,
+        fadeDuration: 1000,
+        positionFromTop: 300,
+    });
+
+    // top
+    $('.top').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+});
